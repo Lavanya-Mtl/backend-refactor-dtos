3b4488c31e7a9ce433be2c1077956cf8297466fe
Lavanya-Mtl <lavanyamatlani1@gmail.com>
2022-12-28 03:06:09 +0530

    dto begins

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,9 @@
+import { DtoBase } from "./dtoBase";
+
+export class UserDto extends DtoBase {
+    firstname?: string;
+    lastname?: string;
+    email?: string;
+    phoneNumber?: string;
+    password?: string
+}
